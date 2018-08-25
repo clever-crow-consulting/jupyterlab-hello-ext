9c988bdc12f66731e32def79ce94ae8f9d541c22
Dan Dye <dye.daniel@gmail.com>
2018-08-25 11:24:20 -0400

    css for comic

--- src/index.ts
+++ src/index.ts
@@ -24,14 +24,28 @@ const extension: JupyterLabPlugin<void> = {
   requires: [ICommandPalette],
   activate: (app: JupyterLab, palette: ICommandPalette) => {
     console.log('extension hellolabext starting...');
+
+    // Create a single widget
     let widget: Widget = new Widget();
     widget.id = 'xkcd-jupyterlab';
     widget.title.label = 'xkcd.com';
     widget.title.closable = true;
+    widget.addClass('jp-xkcdWidget');
+
 
     // Add an image element to the panel
     let img = document.createElement('img');
+    img.className = 'jp-xkcdCartoon';
     widget.node.appendChild(img);
+
+    // New: add an attribution badge
+    img.insertAdjacentHTML('afterend',
+      `<div class="jp-xkcdAttribution">
+        <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-xkcdAttribution" target="_blank">
+          <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
+        </a>
+      </div>`
+    );
 
     // Fetch info about a random comic
     fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
