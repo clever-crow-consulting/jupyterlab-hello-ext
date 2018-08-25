8d85044d92b729996f27071f68b22e92ec6c3c18
Dan Dye <dye.daniel@gmail.com>
2018-08-25 11:18:08 -0400

    display comic working

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 
 import {
+  Widget
+} from '@phosphor/widgets';
+
+import {
   ICommandPalette
 } from '@jupyterlab/apputils';
 
@@ -14,12 +18,47 @@ import '../style/index.css';
  * Initialization data for the hellolabext extension.
  */
 const extension: JupyterLabPlugin<void> = {
+
   id: 'hellolabext',
   autoStart: true,
   requires: [ICommandPalette],
   activate: (app: JupyterLab, palette: ICommandPalette) => {
-    console.log('JupyterLab extension hellolabext is activated!');
-    console.log('ICommandPalette:', palette);
+    console.log('extension hellolabext starting...');
+    let widget: Widget = new Widget();
+    widget.id = 'xkcd-jupyterlab';
+    widget.title.label = 'xkcd.com';
+    widget.title.closable = true;
+
+    // Add an image element to the panel
+    let img = document.createElement('img');
+    widget.node.appendChild(img);
+
+    // Fetch info about a random comic
+    fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
+      return response.json();
+    }).then(data => {
+      img.src = data.img;
+      img.alt = data.title;
+      img.title = data.alt;
+    });
+
+    // Add an application command
+    const command: string = 'xkcd:open';
+    app.commands.addCommand(command, {
+      label: 'Random xkcd comic',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.addToMainArea(widget);
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({command, category: 'Tutorial'});
+    console.log('Done.');
   }
 };
 
